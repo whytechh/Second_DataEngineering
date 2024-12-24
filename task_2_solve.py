c766000796217f61b7a53b46b4019b3c8bf531c4
whytechh <whytechh@yandex.com>
2024-12-24 20:35:46 +0500

    Update task_2_solve.py

--- task_2_solve.py
+++ task_2_solve.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 
-matrix = np.load('/home/whytech/lab_2/tasks/second_task.npy')
+matrix = np.load('./tasks/second_task.npy')
 
 x = []
 y = []
@@ -15,11 +15,11 @@ for i in range(matrix.shape[0]):
             y.append(j)
             z.append(matrix[i][j])
 
-np.savez('/home/whytech/lab_2/results/task_2_solve.npz', x=x, y=y, z=z)
-np.savez_compressed('/home/whytech/lab_2/results/task_2_solve_compressed.npz', x=x, y=y, z=z)
+np.savez('./results/task_2_solve.npz', x=x, y=y, z=z)
+np.savez_compressed('./results/task_2_solve_compressed.npz', x=x, y=y, z=z)
 
-first_size =  os.path.getsize('/home/whytech/lab_2/results/task_2_solve.npz')
-second_size = os.path.getsize('/home/whytech/lab_2/results/task_2_solve_compressed.npz')
+first_size =  os.path.getsize('./results/task_2_solve.npz')
+second_size = os.path.getsize('./results/task_2_solve_compressed.npz')
 
 print(f'savez = {first_size}')
 print(f'savez_compressed = {second_size}')
